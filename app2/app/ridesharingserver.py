111f0ebb286d796089b9d90daa1e086153a76a44
NEOGIGIO <gigio.forastieri@gmail.com>
2021-08-15 19:12:43 -0300

    cleanup

--- app2/app/ridesharingserver.py
+++ app2/app/ridesharingserver.py
@@ -38,40 +38,6 @@ class RideSharingServer:
                 and ride.get_passengers() == passengers
             )
         ]
-        # self.get_user_object(user_uri).notify_rides(possible_rides)
-
-    def print_ride(self, ride_json):
-        ride = Ride(
-            user_uri=ride_json["user"],
-            from_=ride_json["location"][0],
-            to=ride_json["location"][1],
-            date=ride_json["date"],
-            passengers=ride_json["passengers"],
-            offered=ride_json["offered"],
-            ride_id=ride_json["ride_id"],
-        )
-        if ride.get_is_offered():
-            print(
-                "Motorista {0} tem uma carona de {5} para {1} dia {2} com {3} passageiros (id {4})".format(
-                    self.get_user_object(ride.get_user()).get_name(),
-                    ride.get_location()[1],
-                    ride.get_date(),
-                    ride.get_passengers(),
-                    ride.get_id(),
-                    ride.get_location()[0],
-                )
-            )
-        else:
-            print(
-                "Cliente {0} quer uma carona de {5} para {1} dia {2} para {3} passageiros (id {4})".format(
-                    self.get_user_object(ride.get_user()).get_name(),
-                    ride.get_location()[1],
-                    ride.get_date(),
-                    ride.get_passengers(),
-                    ride.get_id(),
-                    ride.get_location()[0],
-                )
-            )
 
     def get_rides(self):
         return self.rides
@@ -87,28 +53,8 @@ class RideSharingServer:
             ride_id=self.current_id,
         )
         self.current_id = self.current_id + 1
-        # self.print_ride(offered_ride.get_ride_json())
         self.rides.append(offered_ride)
-        # for ride in self.rides:
-        #     if (
-        #         ride.get_location() == offered_ride.get_location()
-        #         and ride.get_date() == offered_ride.get_date()
-        #         and ride.get_passengers() == offered_ride.get_passengers()
-        #         and ride.get_is_offered() == 0
-        #     ):
-        #         try:
-        #             passenger_uri = ride.get_user()
-        #             # self.get_user_object(passenger_uri).notify_rides(
-        #             #     [offered_ride.get_ride_json()]
-        #             # )
-
-        #             # driver_uri = offered_ride.get_user()
-        #             # self.get_user_object(driver_uri).notify_rides(
-        #             #     [ride.get_ride_json()]
-        #             # )
-        #         except:
-        #             pass
-
+        
         return offered_ride.get_id()
 
     def add_wanted_ride(self, ride_json):
@@ -122,27 +68,7 @@ class RideSharingServer:
             ride_id=self.current_id,
         )
         self.current_id = self.current_id + 1
-        # self.print_ride(wanted_ride.get_ride_json())
         self.rides.append(wanted_ride)
-        # for ride in self.rides:
-        #     if (
-        #         ride.get_location() == wanted_ride.get_location()
-        #         and ride.get_date() == wanted_ride.get_date()
-        #         and ride.get_passengers() == wanted_ride.get_passengers()
-        #         and ride.get_is_offered() == 1
-        #     ):
-        #         try:
-        #             driver_uri = ride.get_user()
-        #             # self.get_user_object(driver_uri).notify_rides(
-        #             #     [wanted_ride.get_ride_json()]
-        #             # )
-
-        #             # passenger_uri = wanted_ride.get_user()
-        #             # self.get_user_object(passenger_uri).notify_rides(
-        #             #     [ride.get_ride_json()]
-        #             # )
-        #         except:
-        #             pass
 
         return wanted_ride.get_id()
 
--- v2client.js
+++
@@ -1,7 +0,0 @@
-var EventSource = require("eventsource");
-
-var source = new EventSource("/send");
-source.addEventListener('greeting', function(event) {
-    var data = JSON.parse(event.data);
-    // do what you want with this data
-}, false);
